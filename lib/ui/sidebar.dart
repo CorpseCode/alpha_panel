44995707b68bf1d17089742ad621d8b7f9a3c7ca
CorpseCode <pushkarupadhyay2004@gmail.com>
2025-12-07 11:58:17 +0530

    Added sidebar

---
+++ lib/ui/sidebar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:alpha/providers/sidebar_provider.dart';
+
+class SidebarOverlay extends ConsumerWidget {
+  const SidebarOverlay({super.key, required this.child});
+
+  final Widget child; // This will be your column of buttons etc.
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final visible = ref.watch(sidebarProvider);
+
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+      left: visible ? 0 : -450,
+      top: 54,
+      bottom: 0,
+      width: 400,
+      child: child,
+    );
+  }
+}
